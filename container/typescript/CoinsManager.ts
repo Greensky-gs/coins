955500c3647d2c022ab6dcdabedcd6918417fb62
Greensky-gs <72513011+Greensky-gs@users.noreply.github.com>
2022-10-22 09:27:49 +0200

    feat(manager): :sparkles: Make manager in typescript

--- container/typescript/CoinsManager.ts
+++ container/typescript/CoinsManager.ts
@@ -36,11 +36,77 @@ export class CoinsManager {
         data.coins+= Math.abs(coins);
         this.cache.set(user_id, data);
         this.save(user_id, ex);
-        
+
         return data;
     }
-    public getUserData(user_id: string): coinsData | undefined {
+    public removeCoins(user_id: string, coins: number) {
+        const data = this.getUserData(user_id);
+        if (!data) return 'not exists';
+
+        if (Math.abs(coins) > data.coins) return false;
+        data.coins -= Math.abs(coins);
+
+        this.cache.set(user_id, data);
+        this.save(user_id, true);
+        return true;
+    }
+    public deposit(user_id: string, coins: number) {
+        const data = this.getUserData(user_id);
+        if (!data) return 'not exists';
+
+        if (Math.abs(coins) > data.coins) return false;
+        data.coins -= Math.abs(coins);
+        data.bank+= Math.abs(coins);
+        this.cache.set(user_id, data);
+        this.save(user_id, true);
+
+        return true;
+    }
+    public withdraw(user_id: string, coins: number) {
+        const data = this.getUserData(user_id);
+        if (!data) return 'not exists';
+
+        if (Math.abs(coins) > data.bank) return false;
+        data.coins+=Math.abs(coins);
+        data.bank -= Math.abs(coins);
+        this.cache.set(user_id, data);
+
+        this.save(user_id, true);
+        return true;
+    }
+    private getUserData(user_id: string): coinsData | undefined {
         return this.cache.get(user_id);
+    }
+    public stats = this.getUserData
+    public get leaderboard() {
+        const array: coinsData[] = [];
+        this.cache.forEach((v) => {
+            array.push(v);
+            array.sort((a, b) => (a.coins + a.bank) - (b.coins + b.coins));
+        });
+
+        return array;
+    }
+    public pay(options: { donator: string, receiver: string, amount: number }): Promise<'not exists' | boolean> {
+        return new Promise<'not exists' | boolean>(async(resolve) => {
+            const ex = this.exists(options.receiver);
+
+            const donator = this.stats(options.donator);
+            const receiver = this.stats(options.receiver) ?? Object.assign(this.config, { user_id: options.receiver });
+    
+            if (!donator) return resolve('not exists');
+            if (Math.abs(options.amount) > donator.coins) return resolve(false);
+            
+            receiver.coins += Math.abs(options.amount);            
+            this.cache.set(options.receiver, receiver);
+            await this.save(options.receiver, ex);
+
+            donator.coins -= Math.abs(options.amount);
+            this.cache.set(options.donator, donator);
+            await this.save(options.donator, true);
+
+            return resolve(true);
+        });
     }
     private get config() {
         return {
@@ -51,11 +117,11 @@ export class CoinsManager {
     private exists(user_id: string): boolean {
         return this.cache.has(user_id);
     }
-    private save(user_id: string, exists: boolean) {
+    private async save(user_id: string, exists: boolean) {
         const data = this.cache.get(user_id);
         const sql = exists ? `UPDATE coins SET coins='${data?.coins}', bank='${data?.bank}' WHERE user_id='${user_id}'` : `INSERT INTO coins (user_id, coins, bank) VALUES ('${user_id}', '${data?.coins}', '${data?.bank}')`;
 
-        query(sql);
+        await query(sql);
     }
     private async fillCache() {
         const coins = await query<coinsData>(`SELECT * FROM coins`);
